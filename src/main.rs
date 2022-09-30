da24d7232dba59b26a387fa86291de58e2bd4805
Joshua Snyder <joshua@timenavi.com>
2022-09-30 22:36:15 +0100

    updates

--- src/main.rs
+++ src/main.rs
@@ -51,29 +51,34 @@ impl App {
             error!("Could not add a block - invalid")
         }
     }
+
+    fn is_block_valid(&self, block: &Block, previous_bloc: &Block) -> bool {
+        let DIFFICULTY_PREFIX = "00";
+
+        if block.previous_hash != previous_block.hash {
+            warn!("block with id: {}, has wrong previous hash", block.id);
+            return false;
+        } else if !hash_to_binary_representation(
+            &hex::decode(&block.hash).expect("can decode from hex"),
+        )
+        .starts_with(DIFFICULTY_PREFIX)
+        {
+            warn!("block with id: {}, has invalid difficulty", block.id);
+            return false;
+        } else if (block.id != previous_block.id + 1) {
+            warn!("block with id: {}, has invalid id", block.id);
+            return false;
+        } else if hex::encode(calculate_hash(
+            block.id,
+            block.timestamp,
+            &block.previous_hash,
+            &block.data,
+            block.nonce,
+        )) != block.hash
+        {
+            warn!("block with id: {} has invalid hash", block.id);
+            return false;
+        }
+        true
+    }
 }
-// impl App {
-//     fn new() -> Self {
-//         Self { blocks: vec![] }
-//     }
-
-//     fn genesis(&mut self) {
-//         let genesis_block = Block {
-//             id: 0,
-//             timestamp: Utc::now().timestamp(),
-//             previous_hash: String::from("genesis"),
-//             data: String::from("genesis!"),
-//             nonce: 2836,
-//             hash: "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43".to_string(),
-//         };
-//         self.blocks.push(genesis_block);
-//     }
-
-// fn try_add_block(&mut self, block: Block) {
-//     let latest_block = self.blocks.last().expect("there is at least one block");
-//     if self.is_block_valid(&block, latest_block) {
-//         self.blocks.push(block);
-//     } else {
-//         error!("could not add block - invalid");
-//     }
-// }
